3729349cce915a6808936cb11bc1b59e103b0809
Cameron Earl <cameron.a.earl@gmail.com>
2024-01-14 13:50:20 -0500

    Tests Methods and Creates Files: Calls method to retrieve file names, in main application Creates the webcontroller for the /index route, and creates the index.html file it will return

--- src/main/java/cameroncodes/musicarchivebackend/MusicarchivebackendApplication.java
+++ src/main/java/cameroncodes/musicarchivebackend/MusicarchivebackendApplication.java
@@ -1,13 +1,26 @@
 package cameroncodes.musicarchivebackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import cameroncodes.musicarchivebackend.services.StorageService;
+
+
 
 @SpringBootApplication
 public class MusicarchivebackendApplication {
 
+	@Autowired
+	private static StorageService storageService;
+
 	public static void main(String[] args) {
-		SpringApplication.run(MusicarchivebackendApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MusicarchivebackendApplication.class, args);
+
+		StorageService storageService = context.getBean(StorageService.class);
+
+		System.out.println(storageService.getSongFileNames());
 	}
 
 }
